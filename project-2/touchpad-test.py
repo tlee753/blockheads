0fad0fe201df1cc13a3dbe4c50f950054d8cd172
lara-juniper <lara.orlandic@gmail.com>
2018-11-12 18:55:15 -0500

    Updating touchpad test

--- project-2/touchpad-test.py
+++ project-2/touchpad-test.py
@@ -1,23 +1,27 @@
+import sys
 import time
-import board
-import busio
-# Import MPR121 module.
-import adafruit_mpr121
- 
-# Create I2C bus.
-i2c = busio.I2C(board.SCL, board.SDA)
- 
+
+import Adafruit_MPR121.MPR121 as MPR121
 # Create MPR121 class.
-mpr121 = adafruit_mpr121.MPR121(i2c)
+
+cap = MPR121.MPR121()
 # Note you can optionally change the address of the device:
 #mpr121 = adafruit_mpr121.MPR121(i2c, address=0x91)
  
-# Loop forever testing each input and printing when they're touched.
+last_touched = cap.touched()
 while True:
-    # Loop through all 12 inputs (0-11).
+    current_touched = cap.touched()
+    # Check each pin's last and current state to see if it was pressed or released.
     for i in range(12):
-        # Call is_touched and pass it then number of the input.  If it's touched
-        # it will return True, otherwise it will return False.
-        if mpr121[i].value:
-            print('Input {} touched!'.format(i))
-    time.sleep(0.25)  # Small delay to keep from spamming output messages.
+        # Each pin is represented by a bit in the touched value.  A value of 1
+        # means the pin is being touched, and 0 means it is not being touched.
+        pin_bit = 1 << i
+        # First check if transitioned from not touched to touched.
+        if current_touched & pin_bit and not last_touched & pin_bit:
+            print('{0} touched!'.format(i))
+        # Next check if transitioned from touched to not touched.
+        if not current_touched & pin_bit and last_touched & pin_bit:
+            print('{0} released!'.format(i))
+    # Update last state and wait a short period before repeating.
+    last_touched = current_touched
+    time.sleep(0.1)
