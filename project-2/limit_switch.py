74044802df9c12ee784383edc90fccefe07bcac9
lara-juniper <lara.orlandic@gmail.com>
2018-11-12 19:30:45 -0500

    fixing limit switch code

--- project-2/limit_switch.py
+++ project-2/limit_switch.py
@@ -1,9 +1,10 @@
 import RPi.GPIO as GPIO
 
+Limit_switch_channel = 26;
 GPIO.setwarnings(False) # Ignore warning for now
 GPIO.setmode(GPIO.BOARD) # Use physical pin numbering
-GPIO.setup(10, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) # Set pin 10 to be an input pin and set initial value to be pulled low (off)
+GPIO.setup(Limit_switch_channel, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) # Set pin 10 to be an input pin and set initial value to be pulled low (off)
 
 while True: # Run forever
-    if GPIO.input(26) == GPIO.HIGH:
+    if GPIO.input(Limit_switch_channel) == GPIO.HIGH:
         print("Button was pushed!")
