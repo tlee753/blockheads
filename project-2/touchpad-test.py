dc198e55f7f9a402d24720c36dbe15df85f3f3d9
lara-juniper <lara.orlandic@gmail.com>
2018-11-12 19:17:50 -0500

    Updating touchpad code for our purposes

--- project-2/touchpad-test.py
+++ project-2/touchpad-test.py
@@ -2,12 +2,22 @@ import sys
 import time
 
 import Adafruit_MPR121.MPR121 as MPR121
-# Create MPR121 class.
 
+
+print('Adafruit MPR121 Capacitive Touch Sensor Test')
+
+# Create MPR121 instance.
 cap = MPR121.MPR121()
-# Note you can optionally change the address of the device:
-#mpr121 = adafruit_mpr121.MPR121(i2c, address=0x91)
- 
+
+# Initialize communication with MPR121 using default I2C bus of device, and
+# default I2C address (0x5A).  On BeagleBone Black will default to I2C bus 0.
+if not cap.begin():
+    print('Error initializing MPR121.  Check your wiring!')
+    sys.exit(1)
+
+correct_combination = '1234';
+correct_numbers_entered = 0;
+   
 last_touched = cap.touched()
 while True:
     current_touched = cap.touched()
@@ -18,10 +28,16 @@ while True:
         pin_bit = 1 << i
         # First check if transitioned from not touched to touched.
         if current_touched & pin_bit and not last_touched & pin_bit:
-            print('{0} touched!'.format(i))
+            #print('{0} touched!'.format(i))
+           if i == int(correct_combination[correct_numbers_entered]):
+              print('Correct number entered!')
+              correct_numbers_entered = correct_numbers_entered + 1;
+              if correct_numbers_entered == 3:
+                print('Unlocked!!')
+                correct_numbers_entered = 0;
         # Next check if transitioned from touched to not touched.
-        if not current_touched & pin_bit and last_touched & pin_bit:
-            print('{0} released!'.format(i))
+        #if not current_touched & pin_bit and last_touched & pin_bit:
+            #print('{0} released!'.format(i))
     # Update last state and wait a short period before repeating.
     last_touched = current_touched
     time.sleep(0.1)
