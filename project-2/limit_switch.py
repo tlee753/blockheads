43554adebca8b7c36aea663ba189b7173846d70c
lara-juniper <lara.orlandic@gmail.com>
2018-11-12 19:48:08 -0500

    Update limit_switch.py

--- project-2/limit_switch.py
+++ project-2/limit_switch.py
@@ -5,7 +5,7 @@ GPIO.setmode(GPIO.BCM)
 
 Pin_number = 26
 
-GPIO.setup(Pin_number, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(Pin_number, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 
 while True:
     input_state = GPIO.input(Pin_number)
