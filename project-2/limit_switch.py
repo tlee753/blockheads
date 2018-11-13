882f2a91e1bf96c2cbad37ddcef1d3a1aa649599
lara-juniper <lara.orlandic@gmail.com>
2018-11-12 19:40:57 -0500

    Update limit_switch.py

--- project-2/limit_switch.py
+++ project-2/limit_switch.py
@@ -3,7 +3,7 @@ import time
 
 GPIO.setmode(GPIO.BCM)
 
-GPIO.setup(18, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(18, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 
 while True:
     input_state = GPIO.input(18)
