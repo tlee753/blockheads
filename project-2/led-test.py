9aa012d1530675e4dff9d7b3915eda1063fe9e37
lara-juniper <lara.orlandic@gmail.com>
2018-11-12 20:11:57 -0500

    Create led-test.py

---
+++ project-2/led-test.py
@@ -0,0 +1,47 @@
+import sys, time
+
+import RPi.GPIO as GPIO
+
+redPin   = 11
+greenPin = 13
+bluePin  = 15
+
+while True:
+  redOn()
+  time.sleep(1)
+  redOff()
+  greenOn()
+  time.sleep(1)
+  greenOff()
+  blueOn()
+  time.sleep(1)
+  blueOff()
+  
+
+def blink(pin):
+  GPIO.setmode(GPIO.BOARD)
+  GPIO.setup(pin, GPIO.OUT)
+  GPIO.output(pin, GPIO.HIGH)
+  
+def turnOff(pin):
+	GPIO.setmode(GPIO.BOARD)
+	GPIO.setup(pin, GPIO.OUT)
+	GPIO.output(pin, GPIO.LOW)
+  
+def redOn():
+	blink(redPin)
+  
+def greenOn():
+  blink(greenPin)
+  
+def blueOn():
+  blink(bluePin)
+  
+def redOff():
+	turnOff(redPin)
+
+def greenOff():
+	turnOff(greenPin)
+
+def blueOff():
+	turnOff(bluePin)
