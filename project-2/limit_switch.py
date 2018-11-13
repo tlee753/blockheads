2095ce169532efc8c6edf6e40de43aae96eb13ab
lara-juniper <lara.orlandic@gmail.com>
2018-11-12 19:46:57 -0500

    Update limit_switch.py

--- project-2/limit_switch.py
+++ project-2/limit_switch.py
@@ -3,10 +3,12 @@ import time
 
 GPIO.setmode(GPIO.BCM)
 
-GPIO.setup(18, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+Pin_number = 26
+
+GPIO.setup(Pin_number, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
 while True:
-    input_state = GPIO.input(18)
+    input_state = GPIO.input(Pin_number)
     if input_state == False:
         print('Button Pressed')
         time.sleep(0.2)
