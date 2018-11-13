d98b92b52e09f548e0a295221ac3ad1f58a8fc7d
lara-juniper <lara.orlandic@gmail.com>
2018-11-12 19:39:23 -0500

    Update limit_switch.py

--- project-2/limit_switch.py
+++ project-2/limit_switch.py
@@ -1,13 +1,12 @@
 import RPi.GPIO as GPIO
 import time
 
-Limit_switch_channel = 26;
-GPIO.setwarnings(False) # Ignore warning for now
-GPIO.setmode(GPIO.BOARD) # Use physical pin numbering
-GPIO.setup(Limit_switch_channel, GPIO.IN, pull_up_down=GPIO.PUD_UP) # Set pin 10 to be an input pin and set initial value to be pulled low (off)
+GPIO.setmode(GPIO.BCM)
 
-while True: # Run forever
-    input_state = GPIO.input(Limit_switch_channel)
+GPIO.setup(18, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+
+while True:
+    input_state = GPIO.input(18)
     if input_state == False:
         print('Button Pressed')
         time.sleep(0.2)
