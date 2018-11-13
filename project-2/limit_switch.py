d166323c014b5cb4140ea8f8c6cc4925d03eba69
lara-juniper <lara.orlandic@gmail.com>
2018-11-12 19:32:55 -0500

    take 3

--- project-2/limit_switch.py
+++ project-2/limit_switch.py
@@ -3,7 +3,7 @@ import RPi.GPIO as GPIO
 Limit_switch_channel = 26;
 GPIO.setwarnings(False) # Ignore warning for now
 GPIO.setmode(GPIO.BOARD) # Use physical pin numbering
-GPIO.setup(Limit_switch_channel, GPIO.IN, pull_up_down=GPIO.PUD_UP) # Set pin 10 to be an input pin and set initial value to be pulled low (off)
+GPIO.setup(Limit_switch_channel, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) # Set pin 10 to be an input pin and set initial value to be pulled low (off)
 
 while True: # Run forever
     if GPIO.input(Limit_switch_channel) == GPIO.HIGH:
